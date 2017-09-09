7eebce784b4e7a8d6d9290ff74219e3aa2fd33d2
kvfrans <kevinfrans2@gmail.com>
2017-09-09 11:27:24 -0400

    Asdsa

--- download_images.py
+++ download_images.py
@@ -15,25 +15,26 @@ for i in range(1000):
     stringreturn = requests.get("http://danbooru.donmai.us/posts.json?tags=translated&limit=20&page=%d" % i)
     posts = stringreturn.json()
     for post in posts:
-        imgurl = "http://danbooru.donmai.us" + post["file_url"]
-        if ("png" in imgurl) or ("jpg" in imgurl):
-            count += 1
-            r = requests.get(imgurl)
-            i = Image.open(io.BytesIO(r.content)).convert('RGB')
-            open_cv_image = np.array(i)
-            img = open_cv_image[:, :, ::-1].copy()
-            boolimage = np.zeros((int(img.shape[0]/32), int(img.shape[1]/32), 3))
-            print("%d %s" % (count, imgurl))
-            notesall = requests.get("http://danbooru.donmai.us/notes.json?group_by=note&search[post_id]=%s" % post["id"]).json()
-            for notes in notesall:
-                x = int(notes["x"])
-                y = int(notes["y"])
-                w = int(notes["width"])
-                h = int(notes["height"])
-                # cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-                boolimage[(y//32):((y+h)//32)+1, (x//32):((x+w)//32)+1, :] = 255;
+        if(file_url in post)
+            imgurl = "http://danbooru.donmai.us" + post["file_url"]
+            if ("png" in imgurl) or ("jpg" in imgurl):
+                count += 1
+                r = requests.get(imgurl)
+                i = Image.open(io.BytesIO(r.content)).convert('RGB')
+                open_cv_image = np.array(i)
+                img = open_cv_image[:, :, ::-1].copy()
+                boolimage = np.zeros((int(img.shape[0]/32), int(img.shape[1]/32), 3))
+                print("%d %s" % (count, imgurl))
+                notesall = requests.get("http://danbooru.donmai.us/notes.json?group_by=note&search[post_id]=%s" % post["id"]).json()
+                for notes in notesall:
+                    x = int(notes["x"])
+                    y = int(notes["y"])
+                    w = int(notes["width"])
+                    h = int(notes["height"])
+                    # cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+                    boolimage[(y//32):((y+h)//32)+1, (x//32):((x+w)//32)+1, :] = 255;
 
-            scipy.misc.imsave("imgs-classes/"+str(count)+"-b.jpg", boolimage)
-            scipy.misc.imsave("imgs/"+str(count)+".jpg", open_cv_image)
-            # cv2.imwrite("imgs/"+str(count)+".jpg", img)
-            # cv2.imwrite("imgs-classes/"+str(count)+"-b.jpg", boolimage)
+                scipy.misc.imsave("imgs-classes/"+str(count)+"-b.jpg", boolimage)
+                scipy.misc.imsave("imgs/"+str(count)+".jpg", open_cv_image)
+                # cv2.imwrite("imgs/"+str(count)+".jpg", img)
+                # cv2.imwrite("imgs-classes/"+str(count)+"-b.jpg", boolimage)
