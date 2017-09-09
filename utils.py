a5a8156d4e3283a9902c70f53a2e02895c740ea0
root <root@ultimate-suwa-chan.c.dcgan-161707.internal>
2017-09-09 21:40:52 +0000

    first train!

--- utils.py
+++ utils.py
@@ -2,11 +2,35 @@ import scipy.misc
 import numpy as np
 import random
 import tensorflow as tf
-import cPickle
 
 
 def get_image(image_path):
-    return transform(imread(image_path))
+    im = imread(image_path)
+    resize = scipy.misc.imresize(im, 512/im.shape[0])
+    if resize.shape[1] < 512:
+        res = np.ones((512,512, 3))
+        res[:resize.shape[0], :resize.shape[1], :] = resize
+    elif resize.shape[1] >= 512:
+        res = resize[:512,:512]
+    res = scipy.misc.imresize(res, (512,512))
+    res = transform(res)
+    return res
+
+def get_image_class(image_path):
+    im = imread(image_path)
+    #print(im.shape)
+    if im.shape[0] > 80 or im.shape[1] > 80:
+        res = np.zeros((16,16))
+    else:
+        resize = scipy.misc.imresize(im, 16/im.shape[0], interp="nearest")
+        if resize.shape[1] < 16:
+            res = np.zeros((16,16))
+            res[:resize.shape[0], :resize.shape[1]] = resize[:,:,0]
+        elif resize.shape[1] >= 16:
+            res = resize[:16, :16, 0]
+    res = scipy.misc.imresize(res, (16,16))
+    res = transform(res)
+    return np.expand_dims(res, 3)
 
 def transform(image):
     return np.array(image)/255.0
@@ -20,7 +44,7 @@ def center_crop(x, crop_h, crop_w=None, resize_w=108):
     return x[j:j+crop_h, i:i+crop_w]
 
 def imread(path):
-    readimage = scipy.misc.imread(path).astype(np.float)
+    readimage = scipy.misc.imread(path,mode="RGB").astype(np.float)
     return readimage
 
 def merge_color(images, size):
@@ -33,12 +57,6 @@ def merge_color(images, size):
         img[j*h:j*h+h, i*w:i*w+w, :] = image
 
     return img
-
-def unpickle(file):
-  fo = open(file, 'rb')
-  dict = cPickle.load(fo)
-  fo.close()
-  return dict
 
 def ims(name, img):
     # print img[:10][:10]
